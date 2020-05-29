129475101e3d340162d497d86f9a5e53746d10c2
shi <shi@163.com>
2020-05-29 14:08:48 +0800

    初始化工程

---
+++ src/main/java/gittest/entity/User.java
@@ -0,0 +1,17 @@
+package gittest.entity;
+
+public class User {
+	private String userName;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public User() {
+		// TODO Auto-generated constructor stub
+	}
+}
---
+++ src/test/java/gittest/UserTest.java
@@ -0,0 +1,11 @@
+package gittest;
+
+import org.junit.Test;
+
+
+public class UserTest {
+	@Test
+	public void test() {
+
+	}
+}
