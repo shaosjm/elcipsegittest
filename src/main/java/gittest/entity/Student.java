2dbcd22cf7f22bd64131b1b44ba0ecf2b47aec58
shi <shi@163.com>
2020-05-29 15:14:10 +0800

    Student

--- src/main/java/gittest/entity/Student.java
+++ src/main/java/gittest/entity/Student.java
@@ -1,5 +1,14 @@
 package gittest.entity;
 
 public class Student {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 
 }
